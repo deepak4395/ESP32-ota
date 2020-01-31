d4311017a0ae35c0c657989626ace6485e449478
deepak4395 <40218711+deepak4395@users.noreply.github.com>
2020-01-31 13:11:14 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -6,15 +6,10 @@ def download_and_install_update_if_available():
     ota_updater.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
 
 def start():
-  print("in Ota");
-    # your custom code goes here. Something like this: ...
-    # from main.x import YourProject
-    # project = YourProject()
-    # ...
+    print("in Ota");    
 
 def boot():
     download_and_install_update_if_available()
     start()
-
-
+    
 boot()
