edd7821c63c02589cd20b0e8c46924fd70aa99eb
deepak4395 <40218711+deepak4395@users.noreply.github.com>
2020-01-31 15:17:51 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,15 +1,26 @@
-from ota_updater import OTAUpdater
+from main.ota_updater import OTAUpdater
+ssid = "M 57"
+password = "8376918157"
+import network
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+    print('connecting to network...')
+    sta_if.active(True)
+    sta_if.connect(ssid, password)
+    while not sta_if.isconnected():
+        pass
+print('network config:', sta_if.ifconfig())
 
-
-def download_and_install_update_if_available():
-    ota_updater = OTAUpdater('url-to-your-github-project')
-    ota_updater.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
+ota_updater = OTAUpdater('https://github.com/deepak4395/ESP32-ota')
+def download_and_install_update_if_available():    
+    ota_updater.download_and_install_update_if_available(ssid,password)
 
 def start():
-    print("in Ota");    
+    print("in Code")
 
 def boot():
     download_and_install_update_if_available()
     start()
-    
+
 boot()
+ota_updater.check_for_update_to_install_during_next_reboot()
